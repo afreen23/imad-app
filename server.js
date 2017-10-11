b64c0a13bae4529b8d0caac2d6ad05bfbc1081fc
afreen24 <studentinsha12@gmail.com>
2017-10-11 21:54:19 +0530

    some ui changes and added article-three locally

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/article-two',function(req,res) {
 });
 
 app.get('/article-three',function(req,res) {
-    res.send('Article three will be served here');
+    res.sendFile(path.join(__dirname,'ui','article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
@@ -33,7 +33,7 @@ app.get('/ui/madi.png', function (req, res) {
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port =80;
+var port =8080;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
