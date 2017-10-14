4f599988771810b87a280bd8f6048d6a6308a16c
afreen24 <studentinsha12@gmail.com>
2017-10-15 00:58:43 +0530

    added counter end point

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.get('/article-three',function(req,res) {
     res.sendFile(path.join(__dirname,'ui','article-three.html'));
 });
 
+app.get('/ui/main.js',function(req,res) {
+    res.sendFile(path.join(__dirname,'ui','main.js'));
+});
+
 var counter=0;
 app.get('/counter',function(req,res) {
     counter+=1;
--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,25 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
+var button=document.getElementById('button');
+button.onclick=function() { 
+	var http=new XMLHttpRequest();
+http.onreadystatechange=function() {
+	if(http.readyState===XMLHttpRequest.DONE){
+		if(http.status===200){
+			var counter=http.responseText;
+	        var span=document.getElementById('count');
+	        span.innerHTML=counter.toString();
+		}
+
+	}
+  }
+http.open('GET','http://localhost:8080/counter',true);
+http.send(); 
+}
+*/
+
+var button=document.getElementById('submit_button');
+button.onclick=function() {
+	console.log("loaded");
+}
+
+
