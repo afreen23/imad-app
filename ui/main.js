841d7714e3932e368ada9016bae5581adb2fd708
afreen23 <studentinsha12@gmail.com>
2017-10-16 00:31:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,14 @@
 console.log('Loaded!');
 var img=document.getElementById('madi');
 var marginLeft=0;
+var interval;
 function moveRight() {
-  marginLeft=marginLeft+1;
+    if(marginLeft==1000) {
+        clearInterval(interval);
+    }
+  marginLeft=marginLeft+5;
   img.style.marginLeft= (marginLeft)+'px';
 }
 img.onclick= function (){
-	var interval=setInterval(moveRight,100);
+	interval=setInterval(moveRight,100);
 }
